d476899e0c4fb7610fe03096ea24061f04c788ed
karpada2 <gongrossman@gmail.com>
2025-07-01 17:14:33 +0300

    added testing files

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/koala_log_testing/KoalaLogTesting.java
@@ -0,0 +1,40 @@
+package org.firstinspires.ftc.teamcode.koala_log_testing;
+
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.config.Config;
+import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import Ori.Coval.Logging.AutoLogManager;
+import Ori.Coval.Logging.Logger.KoalaLog;
+
+@Config
+@TeleOp(name="Koala Log Testing")
+public class KoalaLogTesting extends LinearOpMode {
+    MotorTestingAutoLogged motorTesting;
+
+    public static int position = 0;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        KoalaLog.setup(hardwareMap);
+
+        telemetry = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
+
+        motorTesting = new MotorTestingAutoLogged(hardwareMap, false, "motor");
+
+        waitForStart();
+
+        while (opModeIsActive()) {
+            AutoLogManager.periodic();
+
+            motorTesting.setTargetPosition(position);
+
+            motorTesting.update();
+            
+            telemetry.addLine(motorTesting.getMotorStats());
+            telemetry.update();
+        }
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/koala_log_testing/MotorTesting.java
@@ -0,0 +1,46 @@
+package org.firstinspires.ftc.teamcode.koala_log_testing;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import org.firstinspires.ftc.robotcore.external.navigation.CurrentUnit;
+
+import Ori.Coval.Logging.AutoLog;
+
+@AutoLog
+public class MotorTesting {
+    private DcMotorEx motor;
+
+    public int motorPosition;
+
+    public MotorTesting(HardwareMap hardwareMap, boolean isReversed, String name) {
+        motor = hardwareMap.get(DcMotorEx.class, name);
+        motor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        motor.setDirection(isReversed ? DcMotorSimple.Direction.REVERSE : DcMotorSimple.Direction.FORWARD);
+    }
+
+    public void setTargetPosition(int pos) {
+        motor.setTargetPosition(pos);
+    }
+
+    public void setPower(double power) {
+        motor.setPower(power);
+    }
+
+    public String getMotorStats() {
+        StringBuilder out = new StringBuilder();
+        out.append("current: ");
+        out.append(motor.getCurrent(CurrentUnit.AMPS));
+        out.append(" | position: ");
+        out.append(motor.getCurrentPosition());
+        out.append(" | targetPosition: ");
+        out.append(motor.getTargetPosition());
+        return out.toString();
+    }
+
+    public void update() {
+        motorPosition = motor.getCurrentPosition();
+    }
+}
