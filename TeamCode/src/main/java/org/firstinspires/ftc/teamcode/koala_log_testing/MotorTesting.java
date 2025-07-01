8f4e806e26763b0f2c78da29607d541dc1780387
karpada2 <gongrossman@gmail.com>
2025-07-01 17:02:37 +0300

    fixed repositories and dependencies

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/koala_log_testing/MotorTesting.java
@@ -0,0 +1,10 @@
+package org.firstinspires.ftc.teamcode.koala_log_testing;
+
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+
+import Ori.Coval.Logging.AutoLog;
+
+@AutoLog
+public class Elevator {
+    private DcMotorEx motor;
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/koala_log_testing/TestingOpMode.java
@@ -0,0 +1,4 @@
+package org.firstinspires.ftc.teamcode.koala_log_testing;
+
+public class TestingOpMode {
+}
